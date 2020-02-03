368dbf033e40c0a3bd044b0ae598c05ff527f154
Bogdan Prăzniciuc <bogdan@onis.studio>
2020-02-03 14:41:59 +0000

    Update main.js

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ var data;
 xhr.open("GET", "https://swapi.co/api/");
 xhr.send();
 
+function setData(jsonData) {
+    data = jsonData;
+
+
+}
+
 xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
 
-        data = this.responseText;
+
+        setData(JSON.parse(this.responseText));
     }
 };
 
